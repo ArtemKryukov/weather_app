c3092015d43d3b9a84091343ae7433d396c63445
Artem <nndeveloper.akm@gmail.com>
2022-08-16 14:32:23 +0300

    feat: add show time and getting weather data in console

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ const windSpeed = document.querySelector('.wind_speed')
 const precip = document.querySelector('.precip')
 const pressure = document.querySelector('.pressure')
 const humidity = document.querySelector('.humidity')
+const time = document.querySelector('.time')
+
+
+const showTime = () => {
+    const date = new Date()
+    time.textContent = date.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
+    
+    setTimeout(showTime, 1000)
+}
+showTime()
+// 9c9f8f988a91214b3c811e5c88d080a9
+async function getWeather() {  
+    const url = `http://api.weatherstack.com/current?access_key=9c9f8f988a91214b3c811e5c88d080a9&query=${city.value}`
+    const res = await fetch(url)
+    const data = await res.json() 
+    console.log(data)
+}
+getWeather()
+city.addEventListener('change', getWeather)
+
